cecbeecd0045d50439a43f47f33651b3eb447001
gvega001 <gvega001@gmail.com>
2021-03-03 15:35:38 -0800

    input comment from command line in myMLSapp

--- myMLApp/Program.cs
+++ myMLApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using MyMLAppML.Model;
 
 namespace myMLApp
 {
@@ -6,7 +7,28 @@ namespace myMLApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter the comment:");
+            string line;
+            while ((line = Console.ReadLine()) != null)
+            {
+                string newLine = line.Replace(("").PadRight(0, ' '), "\t");
+                Console.WriteLine(newLine);
+            }
+
+            // Create single instance of sample data from first line of dataset for model input
+            ModelInput sampleData = new ModelInput()
+            {
+                Col0 = line,
+            };
+
+            // Make a single prediction on the sample data and print results
+            var predictionResult = ConsumeModel.Predict(sampleData);
+
+            Console.WriteLine("Using model to make single prediction -- Comparing actual Col1 with predicted Col1 from sample data...\n\n");
+            Console.WriteLine($"Col0: {sampleData.Col0}");
+            Console.WriteLine($"\n\nPredicted Col1 value {predictionResult.Prediction} \nPredicted Col1 scores: [{String.Join(",", predictionResult.Score)}]\n\n");
+            Console.WriteLine("=============== End of process, hit any key to finish ===============");
+            Console.ReadKey();
         }
     }
 }
